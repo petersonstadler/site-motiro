7593f66fceae1432b6a1da926d4ea8552d2b09cc
Peterson Stadler <petersonstadler@gmail.com>
2023-04-28 20:25:01 -0300

    Iniciado a construção da index, migrado home index antiga para controller motiro2022

---
+++ Controllers/Motiro2022Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace site_motiro.Controllers
+{
+    public class Motiro2022Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
